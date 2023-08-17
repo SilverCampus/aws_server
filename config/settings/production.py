77b6b8e127d8bc3bc5f290edcc94caea440889e1
kyubin0314@naver.com <kyubin0314@naver.com>
2023-08-17 21:59:29 +0900

    :sparkles: Rds 연동 추가

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,10 +15,15 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'silvercampusdb',
+        'USER': 'admin',
+        'PASSWORD': 'qwer1234',
+        'HOST': 'silvercampusdb.cucqbp9zzlbn.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
+
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
